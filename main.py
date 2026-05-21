1a657d368cc6824b06ab611987e63e732dbe96c0
vijesharumugam <vijeshvijesh2601@gmail.com>
2026-05-21 13:19:59 +0530

    BaseModel Post the items in Json formate learned and also learned about Exception handeling

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 items = []
 
@@ -9,10 +13,16 @@ def root():
     return {"message": "Hello World"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[:limit]
+    
 
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
+    if item_id < 0 or item_id >= len(items):
+        raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
